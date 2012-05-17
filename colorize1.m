e42bbd459b3b9c05b14e2080556ab571c31935d9
eheiden <eric.heiden@gmail.com>
2012-05-17 07:26:43 -0400

    working on alignment

--- colorize1.m
+++ colorize1.m
@@ -4,10 +4,10 @@ clear all;
 inputs = {'trainCar.jpg', 'river.jpg', 'tree.jpg', 'emir.tif', 'cathedral.tif'};
 outputs = {'trainCarRGB.jpg', 'riverRGB.jpg', 'treeRGB.jpg', 'emirRGB.tif', 'cathedralRGB.tif'};
 
-for i = 1:length(inputs)
+%for i = 1:length(inputs)
     % read in the input image
-    image = imread(inputs{i});
-    %image = imread('trainCar.jpg');
+    %image = imread(inputs{i});
+    image = imread('river.jpg');
     
     % you may want to crop out the black borders first
     
@@ -20,33 +20,49 @@ for i = 1:length(inputs)
     R = image(height*2+1:height*3,:);
 
     % you may want to convert the color planes to double
-    % or do other preprocessing
+    % or do other preprocessing 
     B = im2double(B);
     G = im2double(G);
     R = im2double(R);
     
     %percentage of the image to check for black/white borders
     prct = .07;
-    B_crop = crop_prct(B, prct);
-    G_crop = crop_prct(G, prct);
-    R_crop = crop_prct(R, prct);
-    
-    figure;
-    subplot(1,3,1); imshow(B_crop);
-    subplot(1,3,2); imshow(G_crop);
-    subplot(1,3,3); imshow(R_crop);
+    B = crop_prct(B, prct);
+    G = crop_prct(G, prct);
+    R = crop_prct(R, prct);
+
     % Align the images
     % Functions that might be useful to you for aligning the images include:
     % "circshift", "sum", and "imresize" (for multiscale)
     %%%%%aG = align(G,B);
     %%%%%aR = align(R,B);
+    aG = normxcorr2(B,G);
+    aR = normxcorr2(B,R);
+    
+    absG = abs(aG);
+    absR = abs(aR);
+    
+    [y1,x1] = find(absG == max(absG(:)));
+    maxy = y1;
+    maxx = x1;
+    
+    y1 = y1 - (size(B, 1) - 1)/2;
+    x1 = x1 - (size(B, 2) - 1)/2;
+    imshow(G);
+    hold on
+    plot(x1, y1, 'wo');
+    
+%    R = circshift(R, [11 3]);
+ %   G = circshift(G, [5 2]);
+
+
 
     % create the color image
     %imageRGB = cat(3, R, G, B);
-    
     %imshow(imageRGB);
+    
     % save it
     %imwrite(imageRGB, outputs{i});
-end
+%end
     
     
