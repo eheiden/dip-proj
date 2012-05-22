2b47605f8a7b30a39222778b13b35cc1f7b3f1d1
eheiden <eric.heiden@gmail.com>
2012-05-21 22:18:54 -0400

    align2 working for tree image only

--- align2.m
+++ align2.m
@@ -1,7 +1,63 @@
-function [img] = align2(img, template)
+function [img] = align2(img, template, window)
 %align2 aligns img to template using the sum of squared differences
 %   Warning: Large images will take a long time to process.
 
 
+[rows cols] = size(template);
+
+img_i = img;
+
+%initialize a blank ssd map
+ssd_map = zeros(window,window);
+
+if rows > 1000
+    %resize the images to make computationally easier
+    img_sml = imresize(img_i,.25);
+    template_sml = imresize(template,.25);
+
+    [rows cols] = size(template_sml);
+    %dimensions for the template
+    rows3rd = floor(rows/3);
+    rows16th = floor(rows/16);
+    cols3rd = floor(cols/3);
+    cols16th = floor(cols/16);
+    
+    for i=1:window
+        for j=1:window
+            imgshift = circshift(img_i,[i-ceil(window/2) j-ceil(window/2)]);
+            template_chop = template_sml(rows16th:rows3rd,cols16th:cols3rd);
+            img_chop = imgshift(rows16th:rows3rd,cols16th:cols3rd);
+            ssd_map(i,j) = sum(((template_chop(:)-img_chop(:)).^2));
+        end
+    end
+    
+    [ssd_ypeak,ssd_xpeak] = find(ssd_map == min(ssd_map(:)));
+    ssd_ypeak = floor(ssd_ypeak-window/2)*4;
+    ssd_xpeak = floor(ssd_xpeak-window/2)*4;
+
+else
+    %dimensions for the template
+    rows3rd = floor(rows/3);
+    rows16th = floor(rows/16);
+    cols3rd = floor(cols/3);
+    cols16th = floor(cols/16);
+    
+    %create a SSD map of size window
+    for i=1:window
+        for j=1:window
+            imgshift = circshift(img_i,[i-ceil(window/2) j-ceil(window/2)]);
+            template_chop = template(rows16th:rows3rd,cols16th:cols3rd);
+            img_chop = imgshift(rows16th:rows3rd,cols16th:cols3rd);
+            ssd_map(i,j) = sum(((template_chop(:)-img_chop(:)).^2));
+        end
+    end
+    
+    %find lowest ssd in window and determine the shift based on that
+    [ssd_ypeak,ssd_xpeak] = find(ssd_map == min(ssd_map(:)));
+    ssd_ypeak = floor(ssd_ypeak-window/2);
+    ssd_xpeak = floor(ssd_xpeak-window/2);
+end
+
+img = circshift(img,[ssd_ypeak ssd_xpeak]);
 end
 
---
+++ colorize2.m
@@ -0,0 +1,50 @@
+close all;
+
+dbstop if error
+
+inputs = {'trainCar.jpg', 'river.jpg', 'tree.jpg', 'emir.tif', 'cathedral.tif'};
+outputs = {'trainCarRGB.jpg', 'riverRGB.jpg', 'treeRGB.jpg', 'emirRGB.tif', 'cathedralRGB.tif'};
+
+for i = 1:length(inputs)
+    % read in the input image
+    image = imread(inputs{i});
+        
+    % you may want to crop out the black borders first
+    
+    % compute the height of each part (just 1/3 of total)
+    height = floor(size(image,1)/3);
+    
+    % separate color channels
+    B = image(1:height,:);
+    G = image(height+1:height*2,:);
+    R = image(height*2+1:height*3,:);
+
+    % you may want to convert the color planes to double
+    % or do other preprocessing 
+    B = im2double(B);
+    G = im2double(G);
+    R = im2double(R);
+    
+    %percentage of the image to check for black/white borders
+    prct = .08;
+    B = crop_prct(B, prct);
+    G = crop_prct(G, prct);
+    R = crop_prct(R, prct);
+
+    %align the green channel to blue:
+    G = align2(G,B,40);
+    %align the red channel to blue:
+    R = align2(R,B,40);
+
+    prct = .05;
+    B = crop_prct(B, prct);
+    G = crop_prct(G, prct);
+    R = crop_prct(R, prct);
+    
+    % create the color image
+    imageRGB = cat(3, R, G, B);
+    imshow(imageRGB);
+
+    % save it
+    imwrite(imageRGB, outputs{i});
+end
